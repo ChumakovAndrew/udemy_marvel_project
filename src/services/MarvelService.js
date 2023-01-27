d1a941045c64829df8af0291b0a09b4057bc24c9
AndrewChumakov <andrew.chumakov12@gmail.com>
2023-01-27 22:41:54 +0300

    добавлен MarvelService

---
+++ src/services/MarvelService.js
@@ -0,0 +1,16 @@
+
+class MarvelSevice {
+    getResourse = async (url) => {
+        let res = await fetch(url);
+
+        if(!res.ok){
+            throw new Error (`Could not fetch ${url}, status: ${res.status}`)
+        }
+
+        return await res.json();
+    }
+
+    
+}
+
+export default MarvelSevice
