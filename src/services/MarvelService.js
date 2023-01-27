f092adc6c18aa17deb1564bf089b98a50d41e75b
AndrewChumakov <andrew.chumakov12@gmail.com>
2023-01-27 22:44:52 +0300

    добавлен запрос на получение всех персонажей getAllCharacters

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -10,6 +10,9 @@ class MarvelSevice {
         return await res.json();
     }
 
+    getAllCharacters = () => {
+        return this.getResourse("https://gateway.marvel.com:443/v1/public/characters?limit=9&offset=210&apikey=ab5709f6705f5f9a400e1448fb37647c")
+    }
     
 }
 
