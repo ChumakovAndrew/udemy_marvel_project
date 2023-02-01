906a9441b5b36cb1b36576532162e52063b8f46a
AndrewChumakov <andrew.chumakov12@gmail.com>
2023-02-01 22:29:27 +0300

    Добавлен метод onCharLoaded и onError

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,6 +1,8 @@
 import { Component } from 'react';
 
 import MarvelService from '../../services/MarvelService';
+import Spiner from '../spiner/Spiner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 
 import './randomChar.scss';
 
@@ -11,52 +13,74 @@ class RandomChar extends Component {
     constructor(props){
         super(props)
         this.state = {
-            char: {}
-    
+            char: {},
+            loading: true,
+            error: false
         }
 
         this.updateChar()
     }
     
-
-
     marvelService = new MarvelService();
 
+    onCharLoaded = (char) => {
+        this.setState({char, loading: false})
+    }
+
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true
+        })
+    }
+    
     updateChar = () => {
         const {getCharacter, _transformCharacter} = this.marvelService
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
         
-        getCharacter(id).then(res => {
+        getCharacter(id)
+            .then(res => {
                 const result = _transformCharacter(res.data.results[0])
-                this.setState({char: result})
+                this.onCharLoaded(result)
             })
+            .catch(this.onError)
     }
 
 
     render() {
-        const {char: {name, description, thumbnail, homepage, wiki }} = this.state
-
+        const { loading, error} = this.state
+        const { char: {name, description, thumbnail, homepage, wiki } } = this.state
+        
         let descr = (description && description.length >= 210) ? description.slice(0, 210) + "..." : description
+
+        const blockRandomChar = <div className="randomchar__block">
+                                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
+                                    <div className="randomchar__info">
+                                        <p className="randomchar__name">{name}</p>
+                                        <p className="randomchar__descr">
+                                            {descr}
+                                        </p>
+                                        <div className="randomchar__btns">
+                                            <a href={homepage} className="button button__main">
+                                                <div className="inner">homepage</div>
+                                            </a>
+                                            <a href={wiki} className="button button__secondary">
+                                                <div className="inner">Wiki</div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+
+        const spiner = loading ? <Spiner/> : null
+        const errorMassage = error ? <ErrorMessage/> : null
+        
+
 
         return (
             <div className="randomchar">
-                <div className="randomchar__block">
-                    <img src={thumbnail} alt="Random character" className="randomchar__img"/>
-                    <div className="randomchar__info">
-                        <p className="randomchar__name">{name}</p>
-                        <p className="randomchar__descr">
-                            {descr}
-                        </p>
-                        <div className="randomchar__btns">
-                            <a href={homepage} className="button button__main">
-                                <div className="inner">homepage</div>
-                            </a>
-                            <a href={wiki} className="button button__secondary">
-                                <div className="inner">Wiki</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+                { spiner }
+                { errorMassage }
+                { !(loading || error) ? blockRandomChar : null }
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br/>
