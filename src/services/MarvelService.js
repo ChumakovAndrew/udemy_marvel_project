8a572bec8c5150612241359b5cbc34c99027efee
AndrewChumakov <andrew.chumakov12@gmail.com>
2023-02-20 17:08:41 +0300

    добавленa функция getComic, и доработана функция transformComic

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -20,6 +20,10 @@ const useMarvelService = () => {
         return request(`${_apiBase}comics?limit=${limit}&offset=${offset}&${_apiKey}`)
     }
 
+    const getComic = (id) => {
+        return request(`${_apiBase}comics/${id}?${_apiKey}`)
+    }
+
     const _transformCharacter = (char) => {
         return {
             id: char.id,
@@ -35,14 +39,20 @@ const useMarvelService = () => {
     const transformComics = (comics) => {
         return {
             id: comics.id,
-            title: comics.title,
-            description: comics.description ? comics.description : 'There is no description for this character',
-            thumbnail: comics.thumbnail.path + '.' + comics.thumbnail.extension,
-            prices: comics.prices[0].price
+			title: comics.title,
+			description: comics.description || "There is no description",
+			pageCount: comics.pageCount
+				? `${comics.pageCount} p.`
+				: "No information about the number of pages",
+			thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
+			language: comics.textObjects[0]?.language || "en-us",
+			price: comics.prices[0].price
+				? `${comics.prices[0].price}$`
+				: "not available",
         }
     }
 
-    return {loading, error, clearError, getAllCharacters, getCharacter, _transformCharacter, getAllComics, transformComics}
+    return {loading, error, clearError, getAllCharacters, getCharacter, _transformCharacter, getAllComics, getComic, transformComics}
 }
 
 
